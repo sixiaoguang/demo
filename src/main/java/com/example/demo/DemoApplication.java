746f7b55d750932f2c5280b602cb14ff9dd28a09
sixiaoguang <1173381462>
2018-03-26 22:00:55 +0800

    create project

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping(value="/")
+	@ResponseBody
+	String home(){
+		return "HHHello !";
+	}
+}
---
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping(value="/app")
+@Controller
+public class LoginController {
+
+    @RequestMapping(value="/login",method = RequestMethod.GET)
+    @ResponseBody
+    String login(){
+        return "登录成功";
+    }
+}
